a948a6f0a9830784011bed093668d2d47803ef31
Parbhat <parbhatjaiswal7@gmail.com>
2023-02-04 21:17:45 +0530

    add: new code

---
+++ StickyColumnRow/StickyColumnRow/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  StickyColumnRow
+//
+//  Created by Admin on 28/12/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let reuseID = "CollectionViewCell"
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+}
